5e850aee7a634e379c2e4cbab5c2fe6195b449d4
Lucia Castellanos Paz <lukkskass775@gmail.com>
2026-03-05 14:51:23 +0100

    Python:extraccion de JSON desde log

--- plot_mqtt.py
+++ plot_mqtt.py
@@ -1,10 +1,19 @@
 import json
-import re
 import matplotlib.pyplot as plt
 
-archivo = "mqtt_capture.log"
+archivo="mqtt_capture.log"
 
 with open(archivo, "r") as f:
-    lineas = f.readlines()
+    lineas=f.readlines()
+datos=[]
+for linea in lineas:
+    if "Payload:" in linea:
+        json_str=linea.split("Payload:")[1]
 
-print("Archivo cargado correctamente")
+        try:
+            data=json.loads(json_str)
+            datos.append(data)
+        except:
+            pass
+
+print("nmero de mensajes encontrados:",len(datos))
